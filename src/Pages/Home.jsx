858739275c1a3a3229516290ad1bbace5c3d15c1
mdjameel04 <jameeltony67@gmail.com>
2025-09-09 22:22:28 +0530

    added button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import { assets } from '../assets/assets'
 
 const Home = () => {
@@ -16,6 +17,13 @@ const Home = () => {
         <div className="relative text-center text-white p-8 rounded-xl">
           <h1 className="text-4xl md:text-6xl font-bold">Welcome to my Hotel</h1>
           <p className="mt-4 text-lg">Luxury • Comfort • World-class Services</p>
+          
+         <Link to={'/Service'}>
+         <button className='mt-6 px-6 py-3 bg-gradient-to-r from-blue-500 to-indigo-600 rounded-lg shadow-lg hover:from-blue-600 hover:to-indigo-700 transition '>
+            Explore Services
+         </button>
+         </Link>
+          
         </div>
       </div>
     </div>
